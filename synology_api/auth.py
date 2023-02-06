7ecedf80e0589aae05e807eac2c46af42d71c1e1
MephistoMMM <mephistommm@gmail.com>
2023-02-06 22:15:40 +0800

    feat: cache session in disk

--- synology_api/auth.py
+++ synology_api/auth.py
@@ -2,7 +2,6 @@ import requests
 import os
 import json
 
-from typing_extensions import override
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 class Authentication:
@@ -12,6 +11,7 @@ class Authentication:
         self._username = username
         self._password = password
         self._sid = None
+        self._login_app = ''
         self._session_expire = True
         self._verify = cert_verify
         self._version = dsm_version
@@ -149,8 +149,6 @@ class Authentication:
 
 class CachableAuthentication(Authentication):
 
-    cached_session = {}
-
     def __init__(self, ip_address, port, username, password,
                secure=False,
                cert_verify=False,
@@ -158,28 +156,30 @@ class CachableAuthentication(Authentication):
                debug=True,
                otp_code=None,
                cachedir='~/.cache/synology_api'):
-        super.__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
-        self.cachedir = cachedir
+        super().__init__(
+            ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
+        self.cachedir = os.path.expanduser(cachedir)
         self.cachefile = os.path.join(self.cachedir, "session.json")
+        self.cached_session = {}
 
     def _load(self):
         if not os.path.exists(self.cachedir):
             os.makedirs(self.cachedir, 0o750, True)
         if not os.path.exists(self.cachefile):
-            cached_session = {}
+            self.cached_session = {}
             return
 
         with open(self.cachefile, 'r') as f:
-            cached_session = json.load(f)
+            self.cached_session = json.load(f)
 
     def _store(self):
-        if len(self.cached_session):
+        if len(self.cached_session) < 1:
             return
         if not os.path.exists(self.cachedir):
             os.makedirs(self.cachedir, 0o750, True)
 
         with open(self.cachefile, 'w') as f:
-            cached_session = json.dump(self.cached_session, f)
+            json.dump(self.cached_session, f)
 
     @property
     def session(self) -> dict:
@@ -187,46 +187,63 @@ class CachableAuthentication(Authentication):
             self._load()
         return self.cached_session
 
-    @override
     def get_api_list(self, app=None):
-        api_info = self.session.get('app_api_list')
+        # load api list from cache
         full_info = self.session.get('full_api_list')
-        if (app is not None and api_info is None) or full_info is None:
+        if full_info is None:
+            # request webapi to get full api list
             query_path = 'query.cgi?api=SYNO.API.Info'
             list_query = {'version': '1', 'method': 'query', 'query': 'all'}
 
             response = requests.get(
                 self._base_url + query_path, list_query, verify=self._verify).json()
 
-            if app is not None:
-                self.cached_session['app_api_list'] = {}
-                for key in response['data']:
-                    if app.lower() in key.lower():
-                        self.cached_session['app_api_list'][key] = response['data'][key]
-            else:
-                self.cached_session['full_api_list'] = response['data']
+            full_info = response['data']
+            self.session['full_api_list'] = full_info
+            self._store()
 
-        self.app_api_list = self.cached_session['app_api_list']
-        self.full_api_list = self.cached_session['full_api_list']
-        return
+        self.app_api_list = full_info
+        self.full_api_list = full_info
 
-    @override
     def login(self, application):
-        login_api = 'auth.cgi?api=SYNO.API.Auth'
-        param = {'version': self._version, 'method': 'login', 'account': self._username,
-                 'passwd': self._password, 'session': application, 'format': 'cookie'}
-        if self._otp_code:
-            param['otp_code'] = self._otp_code
+        self._login_app = application
 
-        if not self._session_expire:
-            if self._sid is not None:
-                self._session_expire = False
-                if self._debug is True:
-                    return 'User already logged'
-        else:
-            session_request = requests.get(
-                self._base_url + login_api, param, verify=self._verify)
-            self._sid = session_request.json()['data']['sid']
+        # load sid from cache
+        sid = self.session.get('sids', {}).get(application)
+        if sid is not None :
+            self._sid = sid
             self._session_expire = False
             if self._debug is True:
-                return 'User logging... New session started!'
+                return 'User already logged with cache session id'
+
+        # request webapi to login
+        result = super().login(application)
+        if self.session.get('sids') is None:
+            self.session['sids'] = {}
+        self.session['sids'][application] = self._sid
+        self._store()
+        return result
+
+    def invalid_sid(self, application):
+        self.session['sids'][application] = None
+        self._session_expire = True
+
+    def request_data(self, api_name, api_path, req_param, method=None, response_json=True):
+        response = super().request_data(
+            api_name, api_path, req_param, method, response_json)
+        json_data = response if response_json else response.json()
+
+        if json_data.get('success', False) :
+            return response
+
+        # normal error just return
+        if  119 != json_data.get('error', {}).get('code'):
+            return response
+
+        # invalid session error(119), try to re-login
+        self.invalid_sid(self._login_app)
+        self.login(self._login_app)
+
+        # request again
+        return super().request_data(
+            api_name, api_path, req_param, method, response_json)
--- synology_api/universal_search.py
+++ synology_api/universal_search.py
@@ -6,14 +6,11 @@ from urllib import parse
 
 class UniversalSearch:
     def __init__(self, ip_address, port, username, password, secure=False, cert_verify=False, dsm_version=7, debug=True, otp_code=None):
-        self.session = auth.Authentication(
+        self.session = auth.CachableAuthentication(
             ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
         self.session.login('Finder')
         self.session.get_api_list('Finder')
-        self.request_data = self.session.request_data
         self.finder_list = self.session.app_api_list
-        self._sid = self.session.sid
-        self.base_url = self.session.base_url
 
         if debug is True:
             print('You are now logged in!')
@@ -77,4 +74,4 @@ class UniversalSearch:
             "version": 1
         }
 
-        return self.request_data(api_name, api_path, req_param, method='post')
+        return self.session.request_data(api_name, api_path, req_param, method='post')
